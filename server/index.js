f5807591edb65f56a8904257ef5bcceaa72275bd
Matthew Schrum <mschrum89@gmail.com>
2024-09-04 15:33:34 -0500

    Initial setup for server and client directory structures and database connection

---
+++ server/index.js
@@ -0,0 +1,25 @@
+require("dotenv").config({path: __dirname + "/.env" });
+const express = require('express');
+const pool = require(__dirname + "/config/db.config.js");
+const app = express();
+const PORT = process.env.PORT || 9000;
+
+const getLocations = (req,res) => {
+	pool.query('select * from location where category_id in (2,3)', (error, locations) => {
+		if (error){
+			throw error
+		}
+		
+		res.status(200).json(locations.rows)
+	})
+};
+
+app.get("/", (req,res) => {
+	res.send("Hello World");
+});
+
+app.get('/locations', getLocations)
+
+app.listen(PORT, () => {
+	console.log(`Server listening on the port ${PORT}`);
+});
